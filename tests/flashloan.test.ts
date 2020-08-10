e3bf1905ae785189c92914bd3321b9bdca63448f
ilovelili <route666@live.cn>
2020-08-10 22:52:06 +0900

    unit test working version

--- tests/flashloan.test.ts
+++ tests/flashloan.test.ts
@@ -1,236 +1,175 @@
 require("dotenv").config();
-import {ethers, Contract} from "ethers";
+import {ethers} from "ethers";
 import {KyberNetworkProxyFactory} from "../types/ethers-contracts/KyberNetworkProxyFactory";
-// const Web3 = require("web3");
+import {VaultManagerFactory} from "../types/ethers-contracts/VaultManagerFactory";
+import {ChainId, Token, TokenAmount, Pair} from "@uniswap/sdk";
 
-import Flashloan from "../build/contracts/TestableFlashloan.json";
-import DaiFaucet from "../build/contracts/DaiFaucet.json";
-import VaultManager from "../build/contracts/VaultManager.json";
-
-import abis from "../abis";
 import addresses from "../addresses";
 import chalk from "chalk";
-
-// const {mainnet: addresses} = require("./addresses");
-import {startGanache, networkId, ganacheUri, wallet, provider, deployIdentityContract} from "./ganache";
+import {startGanache, wallet, provider, deployContracts, Addresses, networkId} from "./ganache";
 
 import Ganache from "ganache-core";
-// const web3 = new Web3("http://localhost:8545"); // mainnet fork
+import {Ierc20Factory} from "../types/ethers-contracts/Ierc20Factory";
+import {doesNotMatch} from "assert";
+import {FlashloanFactory} from "../types/ethers-contracts/FlashloanFactory";
 
 jest.setTimeout(100000);
 
-// // start chain (mainnet or mainnet fork)
-// const startChain = async () => {
-//   // returns account address
-//   return web3.eth.accounts.wallet.add(process.env.PRIVATE_KEY).address;
-// };
-
 describe("Test flashloan", () => {
-  // const AMOUNT_ETH = 1;
-  // const RECENT_ETH_PRICE = 300;
-
-  const WETH_AMOUNT = ethers.utils.parseEther("1");
-  const DAI_AMOUNT = ethers.utils.parseEther("10");
-
-  // const DOUBLE_DAI_AMOUNT = web3.utils.toWei(
-  //   (AMOUNT_ETH * RECENT_ETH_PRICE * 2).toString()
-  // );
-
-  // const DIRECTION = {
-  //   KYBER_TO_UNISWAP: 0,
-  //   UNISWAP_TO_KYBER: 1,
-  // };
-
   let account: string;
   let ganacheServer: Ganache.Server;
+  let contractAddresses: Addresses;
 
-  beforeAll(async () => {
+  const daiAddress = addresses.tokens.dai;
+  const wethAddress = addresses.tokens.weth;
+
+  beforeAll(async (done) => {
     ganacheServer = await startGanache();
-    const contractAddress = await deployIdentityContract();
-    console.log(`contact address is ${contractAddress}`);
+    contractAddresses = await deployContracts();
     account = wallet().address;
     console.log(`account address is ${account}`);
+    done();
   });
 
-  afterAll(() => {
+  afterAll((done) => {
     ganacheServer.close();
+    console.log(chalk.green("Test completed. Press ctrl+c to exit"));
+    done();
   });
 
-  test("account should be initialized", async () => {
+  test("account should be initialized", async (done) => {
     expect(account).not.toBe(undefined);
     expect(account).not.toBe("");
-    console.log(`Account balance (ETH): ${ethers.utils.formatEther(await provider().getBalance(account))}`);
+    const balance = ethers.utils.formatEther(await provider().getBalance(account));
+    console.log(`Account balance (ETH): ${balance}`);
+    done();
   });
 
-  test("should get kayber expected rate", async () => {
+  test("should get kayber expected rate", async (done) => {
+    const ethAddress = "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
     const kyber = KyberNetworkProxyFactory.connect(addresses.kyber.kyberNetworkProxy, provider());
-    const daiEthRate = await kyber.getExpectedRate(
-      addresses.tokens.dai,
-      "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
-      DAI_AMOUNT.toString()
-    );
+    const amount = ethers.utils.parseEther("1").toString();
+
+    const daiEthRate = await kyber.getExpectedRate(daiAddress, ethAddress, amount);
+    expect(daiEthRate).not.toBe(null);
     console.log(`Kyber DAI-ETH expectedRate: ${daiEthRate.expectedRate}, slippageRate: ${daiEthRate.slippageRate}`);
 
-    // const kyber = new web3.eth.Contract(
-    //   abis.kyber.kyberNetworkProxy,
-    //   addresses.kyber.kyberNetworkProxy
-    // );
-    // const daiAddress = addresses.tokens.dai;
-    // const daiEthRate = await kyber.methods
-    //   .getExpectedRate(
-    //     daiAddress,
-    //     "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
-    //     DAI_AMOUNT
-    //   )
-    //   .call();
-    // const ethDaiRate = await kyber.methods
-    //   .getExpectedRate(
-    //     "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
-    //     daiAddress,
-    //     DAI_AMOUNT
-    //   )
-    //   .call();
-    // console.log(
-    //   `Kyber DAI-ETH expectedRate: ${daiEthRate.expectedRate}, slippageRate: ${daiEthRate.slippageRate}`
-    // );
-    // console.log(
-    //   `Kyber ETH-DAI expectedRate: ${ethDaiRate.expectedRate}, slippageRate: ${ethDaiRate.slippageRate}`
-    // );
-    // expect(daiEthRate).not.toBe(null);
-    // expect(ethDaiRate).not.toBe(null);
+    const ethDaiRate = await kyber.getExpectedRate(ethAddress, daiAddress, amount);
+    expect(ethDaiRate).not.toBe(null);
+    console.log(`Kyber ETH-DAI expectedRate: ${ethDaiRate.expectedRate}, slippageRate: ${ethDaiRate.slippageRate}`);
+    done();
   });
 
-  test.skip("should get uniswap amount out", async () => {
-    // const {ChainId, Token, TokenAmount, Pair} = require("@uniswap/sdk");
-    // const daiAddress = addresses.tokens.dai;
-    // const wethAddress = addresses.tokens.weth;
-    // const [dai, weth] = await Promise.all(
-    //   [daiAddress, wethAddress].map((tokenAddress) =>
-    //     Token.fetchData(ChainId.MAINNET, tokenAddress)
-    //   )
-    // );
-    // const daiWeth = await Pair.fetchData(dai, weth);
-    // // input weth, get dai
-    // const amountOutDai = daiWeth.getOutputAmount(
-    //   new TokenAmount(weth, WETH_AMOUNT)
-    // );
-    // const amountOutDaiExact = amountOutDai[0].toExact();
-    // console.log(`AmountOut Dai(Exact): ${web3.utils.toWei(amountOutDaiExact)}`);
-    // expect(amountOutDai).not.toBe(null);
+  test("should get uniswap amount out", async (done) => {
+    const amount = ethers.utils.parseEther("1").toString();
+    const [dai, weth] = await Promise.all([daiAddress, wethAddress].map((tokenAddress) => Token.fetchData(ChainId.MAINNET, tokenAddress)));
+    const daiWeth = await Pair.fetchData(dai, weth);
+
+    // input weth, get dai
+    const amountOutDai = daiWeth.getOutputAmount(new TokenAmount(weth, amount));
+    const amountOutDaiExact = amountOutDai[0].toExact();
+    console.log(`AmountOut Dai(Exact): ${amountOutDaiExact}`);
+    expect(amountOutDai).not.toBe(null);
+    done();
   });
 
-  test.skip("should get eth price", async () => {
-    // const kyber = new web3.eth.Contract(
-    //   abis.kyber.kyberNetworkProxy,
-    //   addresses.kyber.kyberNetworkProxy
-    // );
-    // const daiAddress = addresses.tokens.dai;
-    // const ONE_ETH_WEI = web3.utils.toBN(web3.utils.toWei("1"));
-    // const results = await kyber.methods
-    //   .getExpectedRate(
-    //     "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
-    //     daiAddress,
-    //     1
-    //   )
-    //   .call();
-    // const ethPrice = web3.utils
-    //   .toBN("1")
-    //   .mul(web3.utils.toBN(results.expectedRate))
-    //   .div(ONE_ETH_WEI);
-    // console.log(`eth price is ${ethPrice}`);
+  test("should get eth price", async (done) => {
+    const ethAddress = "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
+    const kyber = KyberNetworkProxyFactory.connect(addresses.kyber.kyberNetworkProxy, provider());
+    const amount = ethers.utils.parseEther("1").toString();
+
+    const ethDaiRate = await kyber.getExpectedRate(ethAddress, daiAddress, amount);
+    const ethPrice = ethers.utils.formatEther(ethDaiRate.expectedRate);
+    console.log(`eth price is ${ethPrice}`);
+
+    expect(parseFloat(ethPrice)).toBeGreaterThan(0);
+    done();
   });
 
-  test.skip("borrowing DAI from Maker", async () => {
-    // const dai = new web3.eth.Contract(abis.tokens.erc20, addresses.tokens.dai);
-    // const vaultManager = new web3.eth.Contract(
-    //   VaultManager.abi,
-    //   VaultManager.networks[networkId].address
-    // );
-    // console.log(
-    //   `Borrowing ${web3.utils.fromWei(DOUBLE_DAI_AMOUNT)} DAI from Maker`
-    // );
-    // // the minimum amount of DAI is 20 when you create a vault.
-    // await vaultManager.methods
-    //   .openVault(
-    //     addresses.makerdao.CDP_MANAGER,
-    //     addresses.makerdao.MCD_JUG,
-    //     addresses.makerdao.MCD_JOIN_ETH_A,
-    //     addresses.makerdao.MCD_JOIN_DAI,
-    //     DOUBLE_DAI_AMOUNT
-    //   )
-    //   .send({
-    //     from: admin,
-    //     gas: 2000000,
-    //     gasPrice: 1,
-    //     value: DOUBLE_DAI_AMOUNT,
-    //   });
-    // const daiAdminBalance = await dai.methods.balanceOf(admin).call();
-    // console.log(
-    //   `DAI balance of Your account: ${web3.utils.fromWei(daiAdminBalance)}`
-    // );
-    // expect(daiAdminBalance).toBe(DOUBLE_DAI_AMOUNT);
+  test("borrowing DAI from Maker", async (done) => {
+    const dai = Ierc20Factory.connect(daiAddress, wallet());
+    const vaultManager = VaultManagerFactory.connect(contractAddresses.vaultManager, wallet()); // wallet instead of provider
+
+    const amount = ethers.utils.parseEther("2000");
+    console.log(`Borrowing DAI from Maker`);
+
+    const options = {
+      value: amount,
+      gasLimit: 2000000,
+      gasPrice: 1,
+    };
+
+    const tx = await vaultManager.openVault(
+      addresses.makerdao.CDP_MANAGER,
+      addresses.makerdao.MCD_JUG,
+      addresses.makerdao.MCD_JOIN_ETH_A,
+      addresses.makerdao.MCD_JOIN_DAI,
+      amount,
+      options
+    );
+    await tx.wait();
+
+    console.log(`account is ${account}`);
+    const daiAdminBalance = await dai.balanceOf(account);
+    console.log(`DAI balance of Your account: ${ethers.utils.formatEther(daiAdminBalance)}`);
+
+    expect(daiAdminBalance.toString()).toBe(amount.toString());
+    done();
   });
 
-  test.skip("transfer half of DAI to faucet", async () => {
-    // const dai = new web3.eth.Contract(abis.tokens.erc20, addresses.tokens.dai);
-    // const daiFaucetAddress = DaiFaucet.networks[networkId].address;
-    // // transfer half to faucet while keep half
-    // await dai.methods.transfer(daiFaucetAddress, DAI_AMOUNT).send({
-    //   from: admin,
-    //   gas: 2000000,
-    //   gasPrice: 1,
-    // });
-    // const daiFaucetBalance = await dai.methods
-    //   .balanceOf(daiFaucetAddress)
-    //   .call();
-    // console.log(
-    //   `DAI balance of DaiFaucet: ${web3.utils.fromWei(daiFaucetBalance)}`
-    // );
-    // expect(daiFaucetBalance).toBe(DAI_AMOUNT);
-    // const daiAdminBalance = await dai.methods.balanceOf(admin).call();
-    // console.log(
-    //   `DAI balance of Your account: ${web3.utils.fromWei(daiAdminBalance)}`
-    // );
-    // expect(daiAdminBalance).toBe(DAI_AMOUNT);
+  test("transfer half of DAI to faucet", async (done) => {
+    const dai = Ierc20Factory.connect(daiAddress, wallet());
+    const amount = ethers.utils.parseEther("1000");
+
+    const options = {
+      gasLimit: 2000000,
+      gasPrice: 1,
+    };
+
+    const tx = await dai.transfer(contractAddresses.daiFaucet, amount, options);
+    await tx.wait();
+
+    const daiAdminBalance = await dai.balanceOf(account);
+    console.log(`DAI balance of Your account: ${ethers.utils.formatEther(daiAdminBalance)}`);
+
+    const daiFaucetBalance = await dai.balanceOf(contractAddresses.daiFaucet);
+    console.log(`DAI balance of faucet: ${ethers.utils.formatEther(daiFaucetBalance)}`);
+
+    expect(daiAdminBalance.toString()).toBe(daiFaucetBalance.toString());
+    done();
   });
 
-  test.skip("initiate Flashloan", async () => {
-    // const flashloan = new web3.eth.Contract(
-    //   Flashloan.abi,
-    //   Flashloan.networks[networkId].address
-    // );
-    // console.log("initiating flashloan Kyber => Uniswap");
-    // const logs = await flashloan.methods
-    //   .initateFlashLoan(
-    //     addresses.dydx.solo,
-    //     addresses.tokens.dai,
-    //     DAI_AMOUNT,
-    //     DIRECTION.KYBER_TO_UNISWAP
-    //   )
-    //   .send({
-    //     from: admin,
-    //     gas: 2000000,
-    //     gasPrice: 1,
-    //   });
-    // // console.log(logs);
-    // await logChainEvent(flashloan);
-  });
+  test("initiate Flashloan", async (done) => {
+    const flashloan = FlashloanFactory.connect(contractAddresses.flashloan, wallet());
+    const amount = ethers.utils.parseEther("1000");
 
-  async function logChainEvent(flashloan: any) {
-    // event listener
-    // https://www.pauric.blog/How-to-Query-and-Monitor-Ethereum-Contract-Events-with-Web3/
-    await flashloan
-      .getPastEvents("allEvents", {
-        toBlock: "latest",
-      })
-      .then((logs) => {
-        logs.forEach((log) => {
-          const record = `Event: ${log.event}, Return values: ${JSON.stringify(log.returnValues)}`;
-          console.log(chalk.green(record));
-        });
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  }
+    const direction = {
+      KYBER_TO_UNISWAP: 0,
+      UNISWAP_TO_KYBER: 1,
+    };
+
+    console.log("Initiating flashloan Kyber => Uniswap");
+
+    const options = {
+      gasLimit: 2000000,
+      gasPrice: 1,
+    };
+
+    const tx = await flashloan.initateFlashLoan(addresses.dydx.solo, addresses.tokens.dai, amount, direction.KYBER_TO_UNISWAP, options);
+    await tx.wait();
+
+    const newArbitrage = flashloan.interface.getEvent("NewArbitrage");
+    const logs = await flashloan.provider.getLogs({
+      toBlock: "latest",
+      address: contractAddresses.flashloan,
+      topics: [flashloan.interface.getEventTopic(newArbitrage)],
+    });
+
+    logs.forEach((log) => {
+      const logData = flashloan.interface.parseLog(log);
+      console.log(chalk.green(logData));
+    });
+
+    done();
+  });
 });
--- tests/ganache.ts
+++ tests/ganache.ts
@@ -2,9 +2,12 @@ require("dotenv").config();
 
 import {ethers} from "ethers";
 import truffle from "../truffle-config";
-import FlashloanContract from "../build/contracts/Flashloan.json";
+import FlashloanContract from "../build/contracts/TestableFlashloan.json";
+import VaultManagerContract from "../build/contracts/VaultManager.json";
+import DaiFaucetContract from "../build/contracts/DaiFaucet.json";
 import Ganache from "ganache-core";
 import addresses from "../addresses";
+import chalk from "chalk";
 
 export const network = truffle.networks.mainnetFork;
 export const ganacheUri = `http://${network.host}:${network.port}`;
@@ -29,18 +32,43 @@ export async function startGanache() {
   return server;
 }
 
-export async function deployIdentityContract() {
-  console.log("Deploying Flashloan Contract");
+export async function deployContracts(): Promise<Addresses> {
+  console.log(chalk.green("Deploying VaultManager Contract"));
+  const vaultManagerFactory = new ethers.ContractFactory(VaultManagerContract.abi, VaultManagerContract.bytecode, wallet());
+  const vaultManagerContract = await vaultManagerFactory.deploy();
 
-  const factory = new ethers.ContractFactory(FlashloanContract.abi, FlashloanContract.bytecode, wallet());
-  const contract = await factory.deploy(
+  console.log(chalk.green("Deploying DaiFaucet Contract"));
+  const daiFaucetFactory = new ethers.ContractFactory(DaiFaucetContract.abi, DaiFaucetContract.bytecode, wallet());
+  const daiFaucetContract = await daiFaucetFactory.deploy(addresses.tokens.dai);
+
+  await vaultManagerContract.deployed();
+  await daiFaucetContract.deployed();
+
+  console.log(`VaultManager Address: ${vaultManagerContract.address}`);
+  console.log(`DaiFaucet Address: ${daiFaucetContract.address}`);
+
+  console.log(chalk.green("Deploying Flashloan Contract"));
+  const flashloanFactory = new ethers.ContractFactory(FlashloanContract.abi, FlashloanContract.bytecode, wallet());
+  const flashloanContract = await flashloanFactory.deploy(
     addresses.kyber.kyberNetworkProxy,
     addresses.uniswap.router,
     addresses.tokens.weth,
     addresses.tokens.dai,
+    daiFaucetContract.address,
     wallet().address
   );
 
-  await contract.deployed();
-  return contract.address;
+  await flashloanContract.deployed();
+  console.log(`Flashloan Address: ${flashloanContract.address}`);
+  return {
+    vaultManager: vaultManagerContract.address,
+    daiFaucet: daiFaucetContract.address,
+    flashloan: flashloanContract.address,
+  };
+}
+
+export interface Addresses {
+  vaultManager: string;
+  daiFaucet: string;
+  flashloan: string;
 }
