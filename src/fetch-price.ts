c64cda76ebe563eccf588b1a7b929392b3721ed6
ilovelili <route666@live.cn>
2020-09-19 20:52:37 +0900

    update deps

--- src/fetch-price.ts
+++ src/fetch-price.ts
@@ -15,7 +15,7 @@ const amount_token2 = 1;
 
 const main = async () => {
   rl.question("Select token1 (dai | usdc | weth | * for all)\n", function (token1) {
-    rl.question("Select token2 (eth | bat | knc | lend | link | mkr | susd | * for all)\n", function (token2) {
+    rl.question("Select token2 (eth | bat | knc | lend | link | mkr | susd | * for all)\n", async function (token2) {
       rl.close();
 
       const token1List = Object.keys(addresses.tokens.token1).filter((x) => token1 == "*" || token1 == x);
@@ -35,19 +35,28 @@ const main = async () => {
         return;
       }
 
+      const records: string[] = [];
       for (let token1 of token1List) {
         for (let token2 of token2List) {
           if (token1 == "weth" && token2 == "eth") continue;
           if (token1 == "usdc" && token2 == "susd") continue;
 
-          fetchPrice(token1, token2);
+          const record = await fetchPrice(token1, token2);
+          records.push(record);
         }
       }
+
+      console.log("******************************************");
+      const header = `Token Pair, Kyber Buy, Kyber Sell, Uniswap Buy, Uniswap Sell`;
+      console.log(chalk.magenta(header));
+      records.forEach((r) => {
+        console.log(chalk.blue(r));
+      });
     });
   });
 };
 
-const fetchPrice = async (token1: string, token2: string) => {
+const fetchPrice = async (token1: string, token2: string): Promise<string> => {
   const token1Config = Util.Address.Token1.resolveToken(token1);
   const token2Config = Util.Address.Token2.resolveToken(token2);
 
@@ -57,15 +66,10 @@ const fetchPrice = async (token1: string, token2: string) => {
     const tokenPairRate = 1 / (await Price.getToken1vsToken2Rate(token1Config.address, token2Config.address, provider));
     const uniswapRates = await Price.FetchUniswapRates(provider, tokenPairRate, token1Config, token2Config, amount_token2);
 
-    const header = `${token2}/${token1} Kyber Buy | Sell ***** Uniswap Buy | Sell\n`;
-    const record = `${kyberRates.buy} | ${kyberRates.sell} ***** ${uniswapRates.buy} | ${uniswapRates.sell}`;
-
-    console.log(header);
-    console.log(chalk.blue(record));
-
-    console.log(`\n`);
+    return `${token2}/${token1}, ${kyberRates.buy}, ${kyberRates.sell}, ${uniswapRates.buy}, ${uniswapRates.sell}\n`;
   } catch (e) {
     // usually the stupid "failed to meet quorum"
+    return "";
   }
 };
 
